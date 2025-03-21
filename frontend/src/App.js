b1c32915c8c92fcc63664dfa0190b26670befba7
Derinson <Derinson_valencia23201@elpoli.edu.co>
2025-03-21 15:58:18 -0500

    feat(project): add initial project structure
    Includes public assets, views, and initial backend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+       <h1>Libre Express
+       </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
