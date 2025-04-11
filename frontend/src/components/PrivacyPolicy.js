d12431f1da64f5d0aa8b24faff5c8e6451fc002e
neydercm <neidercorream@hotmail.com>
2025-04-11 00:20:11 -0500

    Agregar ruta y componente para Política de Privacidad

---
+++ frontend/src/components/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+// src/components/PrivacyPolicy.js
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>Política de Privacidad</h2>
+      <p>Esta es la política de privacidad de Libre Express. Aquí explicamos cómo se manejan los datos de los usuarios...</p>
+      {/* Puedes expandir este texto luego */}
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
